390a9112da17ec87cd6c89142b3896a5469997f2
Antonio Scardace <33966051+antonioscardace@users.noreply.github.com>
2023-03-10 01:11:05 +0100

    Add files via upload

--- mysql-db/schema.sql
+++ mysql-db/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS `politics_stats`;
-CREATE DATABASE `politics_stats`;
+CREATE DATABASE `politics_stats` CHARACTER SET UTF8mb4 COLLATE utf8mb4_bin;
 USE `politics_stats`;
 
 CREATE TABLE IF NOT EXISTS `political_groups` (
@@ -14,12 +14,12 @@ CREATE TABLE IF NOT EXISTS `twitter_accounts` (
     `handle` VARCHAR(255) NOT NULL,
     `id` VARCHAR(64) NOT NULL,
     `name` VARCHAR(64) NOT NULL,
-    `img_url` VARCHAR(255) NOT NULL,
+    `profile_image_url` VARCHAR(255) NOT NULL,
     `verified` TINYINT(1) NOT NULL,
     `created_on` VARCHAR(64) NOT NULL,
-    `followers` INT(11) NOT NULL,
-    `following` INT(11) NOT NULL,
-    `total_tweets` INT(11) NOT NULL,
+    `followers_count` INT(11) NOT NULL,
+    `following_count` INT(11) NOT NULL,
+    `total_tweets_count` INT(11) NOT NULL,
     `description` TEXT NOT NULL,
     `political_group` VARCHAR(32) NOT NULL,
     `last_update` DATE NOT NULL,
@@ -30,13 +30,13 @@ CREATE TABLE IF NOT EXISTS `twitter_accounts` (
 CREATE TABLE IF NOT EXISTS `twitter_dailystats_accounts` (
     `handle` VARCHAR(255) NOT NULL,
     `date` DATE NOT NULL,
-    `followers` INT(11) NOT NULL,
-    `fetched_tweets` INT(11) NOT NULL,
+    `followers_count` INT(11) NOT NULL,
+    `fetched_tweets_count` INT(11) NOT NULL,
     `tot_likes` INT(11) NOT NULL,
     `tot_retweets` INT(11) NOT NULL,
     `tot_replies` INT(11) NOT NULL,
-    `avg_likes` DECIMAL(11, 2) NOT NULL,
     `avg_len` DECIMAL(11, 2) NOT NULL,
+    `avg_likes` DECIMAL(11, 2) NOT NULL,
     `avg_retweets` DECIMAL(11, 2) NOT NULL,
     `avg_replies` DECIMAL(11, 2) NOT NULL,
     `avg_sentiment` ENUM('Positive', 'Negative', 'Neutral', 'Null') NOT NULL,
@@ -47,12 +47,13 @@ CREATE TABLE IF NOT EXISTS `twitter_dailystats_accounts` (
 CREATE TABLE IF NOT EXISTS `twitter_dailystats_group` (
     `name` VARCHAR(32) NOT NULL,
     `date` DATE NOT NULL,
-    `followers` INT(11) NOT NULL,
-    `fetched_tweets` INT(11) NOT NULL,
+    `followers_count` INT(11) NOT NULL,
+    `fetched_tweets_count` INT(11) NOT NULL,
     `num_analyzed_accounts` INT(11) NOT NULL,
     `tot_likes` INT(11) NOT NULL,
     `tot_retweets` INT(11) NOT NULL,
     `tot_replies` INT(11) NOT NULL,
+    `tot_tweets` INT(11) NOT NULL,
     `avg_likes` DECIMAL(11, 2) NOT NULL,
     `avg_len` DECIMAL(11, 2) NOT NULL,
     `avg_retweets` DECIMAL(11, 2) NOT NULL,
