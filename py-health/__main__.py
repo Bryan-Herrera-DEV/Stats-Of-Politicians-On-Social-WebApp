7ce82cc3236feabaa8036c283aad6b4bd774d907
Antonio Scardace <33966051+antonioscardace@users.noreply.github.com>
2023-03-24 16:45:32 +0100

    Add files via upload

--- py-health/__main__.py
+++ py-health/__main__.py
@@ -9,12 +9,14 @@ from daemon.checker import Checker
 checker = Checker()
 
 while True:
-    with open(os.environ["SERVERLIST_PATH"]) as dump:
-        for server in full_load(dump)['servers']:
-            checker.scan_server(
-                server['server']['address'],
-                server['server']['contact'],
-                server['server']['contact_type']
-            )
-    
+    if os.environ["IS_RELEASE"] == 1:
+
+        with open(os.environ["SERVERLIST_PATH"]) as dump:
+            for server in full_load(dump)['servers']:
+                checker.scan_server(
+                    server['server']['address'],
+                    server['server']['contact'],
+                    server['server']['contact_type']
+                )
+        
     sleep(int(os.environ["CHECK_TIME_SECONDS"]))
