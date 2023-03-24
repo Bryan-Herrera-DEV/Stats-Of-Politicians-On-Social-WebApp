09ba16a8bd0e6500d7ebeb47efb1d2d5c5730567
Antonio Scardace <33966051+antonioscardace@users.noreply.github.com>
2023-03-24 11:44:44 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 docker compose down
 
 docker compose build mysql-politics
+docker compose build grafana-charts
 docker compose build py-crawler
 docker compose build node-ui
 docker compose build py-health
