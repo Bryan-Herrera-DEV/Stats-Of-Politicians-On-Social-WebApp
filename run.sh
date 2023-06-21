56041acae1ec0530dbf5d9ab7aef04451e1924a8
Antonio Scardace <33966051+antonioscardace@users.noreply.github.com>
2023-06-21 14:41:16 +0200

    Code Smell fix in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@ docker compose build mysql-politics
 docker compose build grafana-charts
 docker compose build py-crawler
 docker compose build node-ui
-docker compose build py-health
 
 docker compose up -d
